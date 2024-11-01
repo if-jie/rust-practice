79645cd6b0e2c9727ea91e738b90675d7cbaa21c
1454164915@qq.com <1454164915@qq.com>
2024-11-01 14:19:17 +0800

    first

---
+++ main.rs/helloworld.rs
@@ -0,0 +1,11 @@
+/*
+ * @Author: 1454164915@qq.com 1454164915@qq.com
+ * @Date: 2024-11-01 13:36:45
+ * @LastEditors: 1454164915@qq.com 1454164915@qq.com
+ * @LastEditTime: 2024-11-01 13:37:12
+ * @FilePath: /learn/rust/main.rs/helloworld.rs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+fn main(){
+    println!("hello world!");
+}
